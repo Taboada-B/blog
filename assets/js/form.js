e8c9958355343ab5bda6a29ef528d95bb7c48392
Bryan Taboada <b.taboada123@gmail.com>
2024-05-18 16:29:38 -0600

    before light/dark modes attempts

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,9 +1,36 @@
 const header = document.querySelector('.header');
+const themeSwitcher = document.querySelector('#theme-switcher')
 const submission = document.querySelector('.submission');
 
 
+// let mode ='light';
+// themeSwitcher.addEventListener('click', function()){
+//     if (mode == 'light'){
+//         mode == 'dark';
+//         container.setAttribute('class', 'dark');
+//     }
+//     else {
+//         mode = 'light';
+//         container.setAttribute('class', 'light')
+//     }
+// }
+
+    // themeSwitcher.addEventListener('click', function () {
+    //     // If mode is dark, apply light background
+    //     if (mode === 'dark') {
+    //       mode = 'light';
+    //       container.setAttribute('class', 'light');
+    //     }
+    //     // If mode is light, apply dark background
+    //     else {
+    //       mode = 'dark';
+    //       container.setAttribute('class', 'dark');
+    //     }
+    //   });
+      
 
 header.children[0].textContent = 'light/dark option soon';
+
 header.children[2].textContent = 'The DevDiary';
 
 
