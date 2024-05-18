77047d9e57b73e1d70ba30623f576559823d7227
Bryan Taboada <b.taboada123@gmail.com>
2024-05-18 16:08:52 -0600

    setting up the dom

---
+++ assets/js/form.js
@@ -0,0 +1,19 @@
+const header = document.querySelector('.header');
+const submission = document.querySelector('.submission');
+
+
+
+header.children[0].textContent = 'light/dark option soon';
+header.children[2].textContent = 'The DevDiary';
+
+
+submission.children[0].textContent = 'Username';
+submission.children[1].textContent = 'userinfo goes here';
+submission.children[2].textContent = 'title';
+submission.children[3].textContent = 'user title goes here';
+submission.children[4].textContent = 'Content';
+submission.children[5].textContent = 'content info and blog goes here';
+submission.children[6].textContent = 'Submit';
+
+
+
