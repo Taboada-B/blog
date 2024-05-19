2e522854adfef578f40340a830440dca19fb2591
Bryan Taboada <b.taboada123@gmail.com>
2024-05-19 09:51:31 -0600

    submission forms

--- assets/js/form.js
+++ assets/js/form.js
@@ -4,9 +4,9 @@ const container = document.querySelector('.container');
 const submission = document.querySelector('.submission');
 
 
-let mode ='light';
-themeSwitcher.addEventListener('click', function(){
-    if (mode === 'light'){
+let mode = 'light';
+themeSwitcher.addEventListener('click', function () {
+    if (mode === 'light') {
         mode = 'dark';
         container.setAttribute('class', 'dark');
     }
@@ -16,32 +16,31 @@ themeSwitcher.addEventListener('click', function(){
     }
 });
 
-    // themeSwitcher.addEventListener('click', function () {
-    //     // If mode is dark, apply light background
-    //     if (mode === 'dark') {
-    //       mode = 'light';
-    //       container.setAttribute('class', 'light');
-    //     }
-    //     // If mode is light, apply dark background
-    //     else {
-    //       mode = 'dark';
-    //       container.setAttribute('class', 'dark');
-    //     }
-    //   });
-      
+// themeSwitcher.addEventListener('click', function () {
+//     // If mode is dark, apply light background
+//     if (mode === 'dark') {
+//       mode = 'light';
+//       container.setAttribute('class', 'light');
+//     }
+//     // If mode is light, apply dark background
+//     else {
+//       mode = 'dark';
+//       container.setAttribute('class', 'dark');
+//     }
+//   });
 
-header.children[0].textContent = 'light/dark option soon';
+
+header.children[0].textContent = 'light/dark';
 
 header.children[2].textContent = 'The DevDiary';
 
-
-submission.children[0].textContent = 'Username';
-submission.children[1].textContent = 'userinfo goes here';
-submission.children[2].textContent = 'title';
-submission.children[3].textContent = 'user title goes here';
-submission.children[4].textContent = 'Content';
-submission.children[5].textContent = 'content info and blog goes here';
-submission.children[6].textContent = 'Submit';
+// submission.children[0].textContent = 'Username';
+// // submission.children[1].textContent = 'userinfo goes here';
+// submission.children[2].textContent = 'title';
+// // submission.children[3].textContent = 'user title goes here';
+// submission.children[4].textContent = 'Content';
+// // submission.children[5].textContent = 'content info and blog goes here';
+// submission.children[6].textContent = 'Submit';
 
 
 
