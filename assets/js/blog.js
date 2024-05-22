5b25fbe34f1783226f7cce04ae62909bac10523b
Bryan Taboada <b.taboada123@gmail.com>
2024-05-21 20:47:35 -0600

    removed comments, added link to my github

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,5 +1,3 @@
-// dark and light mode button
-
 const header = document.querySelector('.header');
 const themeSwitcher = document.querySelector('#theme-switcher');
 const container = document.querySelector('.container');
@@ -21,30 +19,25 @@ themeSwitcher.addEventListener('click', function () {
 
 // end light dark mode button
 
-// making variables from windows localstorage
-// const userName = localStorage.getItem('user-name');
-// const titleName = localStorage.getItem('title-name');
-// const blogContent = localStorage.getItem('blog-content');
 
 //populating the page 
+
 function addUserElement(){
     const userName = localStorage.getItem('user-name');
     // creates a new div
     const newDiv = document.createElement("div");
 // giving div content
 newDiv.textContent = userName;
-
 const targetDiv = document.getElementById('div1');
-
 targetDiv.appendChild(newDiv);
 }
+
 function addTitleElement(){
     const userTitle = localStorage.getItem('title-name');
     // creates a new div
     const newDiv = document.createElement("div");
 // giving div content
 newDiv.textContent = userTitle;
-
 const targetDiv = document.getElementById('div1');
 targetDiv.appendChild(newDiv);
 }
@@ -55,85 +48,11 @@ function addBlogElement(){
     const newDiv = document.createElement("div");
 // giving div content
 newDiv.textContent = userBlog;
-
 const targetDiv = document.getElementById('div1');
-
 targetDiv.appendChild(newDiv);
 }
 
-
+// calling the functions
 addUserElement();
 addTitleElement();
 addBlogElement();
-
-// document.querySelector('#blog-user').textContent = userName;
-// document.querySelector('#title-name').textContent = titleName;
-// document.querySelector('#blog-content').textContent = blogContent;
-
-
-// section.children[0].textContent = '';
-
-
-
-// bellow is rendering things not yet
-
-// function renderBlogContent() {
-//     const user = localStorage.getItem('user');
-//     const title = localStorage.getItem('title');
-//     const blog = localStorage.getItem('blog');
-  
-//     if (!user || !title || !blog) {
-//       return;
-//     }
-  
-//     userSpan.textContent = user;
-//     console.log('first userSpan')
-//     console.log(userSpan)
-//     userTitleSpan.textContent = title;
-//     userBlogSpan.textContent = blog;
-//   }
-
-
-// const userSpan = document.querySelector('#userSpan');
-// const userTitleSpan = document.querySelector('#userTitleSpan');
-// const userBlogSpan = document.querySelector('#userBlogSpan');
-
-
-
-
-// notes below
-
-
-// linking to next page
-// // Create a new anchor element
-// const blogLink = document.createElement('a');
-
-// // Set the href attribute to point to the blog.html page
-// blogLink.href = 'blog.html';
-
-// // Set the inner text of the link
-// blogLink.innerText = 'Visit My Blog';
-
-// // Append the link to a specific element in your existing HTML, for example, a div with id "links"
-// document.getElementById('links').appendChild(blogLink);
-
-
-
-// signUpButton.addEventListener('click', function (event) {
-//     event.preventDefault();
-  
-//     const email = emailInput.value;
-//     const password = passwordInput.value;
-  
-//     if (email === '') {
-//       displayMessage('error', 'Email cannot be blank');
-//     } else if (password === '') {
-//       displayMessage('error', 'Password cannot be blank');
-//     } else {
-//       displayMessage('success', 'Registered successfully');
-  
-//       localStorage.setItem('email', email);
-//       localStorage.setItem('password', password);
-//       renderLastRegistered();
-//     }
-//   });a
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,26 +1,23 @@
-const blogLink = document.createElement('a');
 const msgDiv = document.querySelector('#msg');
 const form = document.getElementById('form');
-// testing to bring up info into spans
+const userInput = document.querySelector('#user');
+const titleInput = document.querySelector('#title');
+const blogInput = document.querySelector('#blog');
+const submit = document.querySelector('#submit')
 
 // entering information and filtering
 function displayMessage(type, message) {
     msgDiv.textContent = message;
     msgDiv.setAttribute('class', type);
   }
-
-const userInput = document.querySelector('#user');
-const titleInput = document.querySelector('#title');
-const blogInput = document.querySelector('#blog');
-const submit = document.querySelector('#submit')
-
+// hitting submit triggers these events
 submit.addEventListener('click',function(event){
 event.preventDefault();
 
 const user = userInput.value;
 const title = titleInput.value;
 const blog = blogInput.value;
-
+// if anything is blank, return an error
 if (user === '') {
     displayMessage('error', 'Username cannot be blank');    
 }
@@ -31,33 +28,16 @@ else if (blog === ''){
     displayMessage('error', 'Blog cannot be blank');
 }
 else {
-    //stores data to browser
+    //stores data to browser under these keys
     window.localStorage.setItem('user-name', user);
     window.localStorage.setItem('title-name', title);
     window.localStorage.setItem('blog-content', blog);
-//go to next page
+//go to next page after hitting submit
 window.location.href = "blog.html";
-
 }
 })
 
-// function renderBlogContent() {
-//     const user = localStorage.getItem('user');
-//     const title = localStorage.getItem('title');
-//     const blog = localStorage.getItem('blog');
-  
-//     if (!user || !title || !blog) {
-//       return;
-//     }
-  
-//     userSpan.textContent = user;
-//     console.log('first userSpan')
-//     console.log(userSpan)
-//     userTitleSpan.textContent = title;
-//     userBlogSpan.textContent = blog;
-//   }
-
-// end information and filtering and populating 
+// end information and filtering
 
 // dark and light mode button
 
@@ -67,9 +47,9 @@ const container = document.querySelector('.container');
 
 header.children[0].textContent = 'light/dark modes';
 
-
 // light dark mode switcher
 let mode = 'light';
+
 themeSwitcher.addEventListener('click', function () {
     if (mode === 'light') {
         mode = 'dark';
@@ -81,21 +61,4 @@ themeSwitcher.addEventListener('click', function () {
     }
 });
 
-
 // end light dark mode button
-
-
-// submit button to change the page
-
-// const submission = document.querySelector('.submission');
-
-// submission.children[0].textContent = 'Username';
-// // submission.children[1].textContent = 'userinfo goes here';
-// submission.children[2].textContent = 'title';
-// // submission.children[3].textContent = 'user title goes here';
-// submission.children[4].textContent = 'Content';
-// // submission.children[5].textContent = 'content info and blog goes here';
-// submission.children[6].textContent = 'Submit';
-
-
-
